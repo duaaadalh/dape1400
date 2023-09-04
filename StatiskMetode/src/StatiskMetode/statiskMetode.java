3c0843b081e8a2fc8227048d18b400c12aa96b27
duaaadalh <duaaalhemdany@gmail.com>
2023-09-04 13:03:19 +0200

    ny klasse

--- StatiskMetode/src/StatiskMetode/statiskMetode.java
+++ StatiskMetode/src/StatiskMetode/statiskMetode.java
@@ -1,4 +1,18 @@
 package StatiskMetode;
 
 public class statiskMetode {
+
+    private static double beregnAreal(double radius){
+          double areal = Math.PI * Math.pow(radius, 2);
+           return areal;
+          //  return Math.PI * Math.pow(radius, 2); --> kunne gjort dette
+    }
+
+    public static void main (String [] args) {
+        double radius = 23.23;
+        double areal = beregnAreal(radius);
+        System.out.println("Arealet til en sirkel med radius" + radius+ " er "+ areal);
+
+
+    }
 }
