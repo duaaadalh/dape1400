ea2350d6f09b2504c71f8f1abd2ac908d939314f
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-09-04 19:47:47 +0200

    endringer

--- KogBoppgave3/src/KogBoppgave3/kogboppgave3.java
+++ KogBoppgave3/src/KogBoppgave3/kogboppgave3.java
@@ -1,4 +1,17 @@
 package KogBoppgave3;
 
+
+class Personer{
+    public String navn;
+    public String adresse;
+    public String telefonnr;
+    public String fødselsår;
+
+
+}
+
+
 public class kogboppgave3 {
+    public static void main(String[] args) {
+    }
 }
