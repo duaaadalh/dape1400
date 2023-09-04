428276348df1066d6e7ec9354202b7187fc1ad86
duaaadalh <duaaalhemdany@gmail.com>
2023-09-04 11:37:51 +0200

    ny mappe

--- KlasserOgObjekter/src/KlasserOgObjekter/klasserOgObjekter.java
+++ KlasserOgObjekter/src/KlasserOgObjekter/klasserOgObjekter.java
@@ -1,4 +1,35 @@
 package KlasserOgObjekter;
 
+class Bil {
+
+    private String farge;
+
+    public void visFarge(){
+    System.out.println("Fargen til bilen er "+ farge);
+
+    }
+
+    public void settFarge(String fargen){
+    farge = fargen;
+
+    }
+
+    public String hentFarge(){
+        return farge;
+    }
+    
+}
+
+
 public class klasserOgObjekter {
+    public static void main (String[] args) {
+    Bil nyBil = new Bil ();
+   // nyBil.farge = "blå";
+        nyBil.settFarge("blå");
+        //nyBil.visFarge();
+        String bilFargen;
+        bilFargen = nyBil.hentFarge();
+        System.out.println("Fargen til bilen er "+ bilFargen);
+
+    }
 }
