1655b7f957ad7d3ebfa9a2760142cd77e7fb13e2
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-09-11 10:07:52 +0200

    endringer

--- KogB2/src/Konstruktor/konstruktor.java
+++ KogB2/src/Konstruktor/konstruktor.java
@@ -1,4 +1,42 @@
 package Konstruktor;
 
+
+class Bil{
+    private String farge;
+    private int HK;
+
+    public Bil(String farge, int HK){
+        this.farge = farge;
+        this.HK = HK;
+    }
+
+    public void setFarge(String farge){
+        this.farge = farge;
+    }
+    public String getFarge(){
+        return farge;
+    }
+
+    public void setHK(int HK){
+        if (HK>0){
+            this.HK = HK;
+        }
+    }
+    public int getHK(){
+        return HK;
+    }
+}
+
+
+
+
+
 public class konstruktor {
+    public static void main(String[] args) {
+        Bil volvo = new Bil("Rød", 234);
+        int HK = volvo.getHK();
+        String farge = volvo.getFarge();
+        System.out.println("Bilens farge er "+ farge+ "."+ " Den har "+ HK+ " HK.");
+
+    }
 }
