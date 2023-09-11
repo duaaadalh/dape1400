ac0290829ff0c7781983233a99aaf27b595a7426
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-09-11 11:18:02 +0200

    oppgaver

--- KogB2/src/bank1/bank1.java
+++ KogB2/src/bank1/bank1.java
@@ -1,7 +1,15 @@
 package bank1;
 
 public class bank1 {
+
     public static void main (String[] args) {
+        konto sparekonto = new konto("12345678910", "Duaa Adnan", 1000);
+        konto lonnskonto = new konto("12345678910","Duaa Adnan", 0);
+        sparekonto.setSaldo(2000);
+        lonnskonto.setSaldo(1000);
+        System.out.println("Sparekontoen tilhører "+ sparekonto.getNavn());
+        System.out.println("Saldoen til denne er nå "+ sparekonto.getSaldo()+ " kroner");
+        System.out.println("Saldoen til lønnskontoen er "+ lonnskonto.getSaldo()+ " kroner");
 
     }
 }
--- KogB2/src/bank1/konto.java
+++ KogB2/src/bank1/konto.java
@@ -4,4 +4,34 @@ public class konto {
     private String personnummer;
     private String navn;
     private double saldo;
+
+    public konto(String personnummer, String navn, double saldo){
+        this.personnummer = personnummer;
+        this.navn = navn;
+        this.saldo = saldo;
+    }
+
+    public String getPersonnummer(){
+        return personnummer;
+    }
+
+    public String getNavn(){
+        return navn;
+    }
+
+    public void setPersonnummer(String personnummer){
+        this.personnummer = personnummer;
+    }
+
+    public void setNavn(String navn){
+        this.navn = navn;
+    }
+
+    public double getSaldo(){
+        return saldo;
+    }
+
+    public void setSaldo(double saldo){
+        this.saldo = saldo;
+    }
 }
