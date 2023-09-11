c8519a31e980b99f5ca18131ab2560eca2f209fa
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-09-11 14:48:00 +0200

    ny oppgave

--- Kontrollstruktorer/src/IfSetninger/ifsetninger.java
+++ Kontrollstruktorer/src/IfSetninger/ifsetninger.java
@@ -1,4 +1,27 @@
 package IfSetninger;
 
+import javax.swing.*;
+
 public class ifsetninger {
-}
+    public static void main(String[] args){
+        String input = JOptionPane.showInputDialog("Skriv inn alderen din");
+        int alder = Integer.parseInt(input);
+        if (alder < 13){
+            JOptionPane.showMessageDialog(null, "Du er et barn");
+        } else if (alder<18) {
+            JOptionPane.showMessageDialog(null, "Du er en ungdom");
+
+        } else if (alder < 70) {
+            JOptionPane.showMessageDialog(null, "Du er voksen");
+            
+        }
+
+        // avslutt alltid med en else dersom du bruke if, else if
+        else {
+            JOptionPane.showMessageDialog(null, "Du er en pensjonist");
+        }
+
+    }
+    }
+
+
