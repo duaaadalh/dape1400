0c1203390ba1b54df00f8b62cdb4be7c77aac47e
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-09-12 15:21:21 +0200

    oppgave 4

--- Kontrollstruktorer2/src/Oppgaver/Oppgave3.java
+++ Kontrollstruktorer2/src/Oppgaver/Oppgave3.java
@@ -1,4 +1,9 @@
 package Oppgaver;
 
 public class Oppgave3 {
+    public static void main(String[] args){
+        for (int i = 0; i < 20; i +=2){
+            System.out.println(i);
+        }
+    }
 }
