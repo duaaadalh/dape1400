44a43ce7b9d2065b009279907a993295bf0a4229
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-09-12 16:20:42 +0200

    oppgave 7

--- Kontrollstruktorer2/src/Oppgaver/Oppgave6.java
+++ Kontrollstruktorer2/src/Oppgaver/Oppgave6.java
@@ -1,4 +1,16 @@
 package Oppgaver;
 
 public class Oppgave6 {
+    public static void main(String[] args){
+        int i = 0;
+        int sum = 0;
+
+        while (i < 10){
+            sum += i;
+            i ++;
+
+        }
+
+        System.out.println("Summene av tallene er " + sum);
+    }
 }
---
+++ Kontrollstruktorer2/src/Oppgaver/Oppgave7.java
@@ -0,0 +1,8 @@
+package Oppgaver;
+
+public class Oppgave7 {
+
+    public static void main(String[] args){
+        
+    }
+}
