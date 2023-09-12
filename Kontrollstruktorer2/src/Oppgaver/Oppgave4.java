fe6356beb3609443a7ff20b1ace1f138a05e3d7f
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-09-12 15:47:23 +0200

    oppgave 5

--- Kontrollstruktorer2/src/Oppgaver/Oppgave4.java
+++ Kontrollstruktorer2/src/Oppgaver/Oppgave4.java
@@ -1,4 +1,17 @@
 package Oppgaver;
 
 public class Oppgave4 {
+    public static void main(String[] args){
+        int sum = 0;
+        int antallPartall = 0;
+        for (int i = 0; i<=100; i +=2){
+            sum += i;
+            antallPartall++;
+        }
+
+        double gjennomsnitt = (double) sum / antallPartall;
+
+        System.out.println("Summen av partallene mellom 0 og 100 er " + sum);
+        System.out.println("Gjennomsnittet av tallene mellom 0 og 99 er "+ gjennomsnitt);
+    }
 }
