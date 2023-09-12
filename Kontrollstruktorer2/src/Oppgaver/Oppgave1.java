8f9be40638dfc5fa87551eaf711d128d0b4dbaec
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-09-12 13:56:42 +0200

    oppgave 2

--- Kontrollstruktorer2/src/Oppgaver/Oppgave1.java
+++ Kontrollstruktorer2/src/Oppgaver/Oppgave1.java
@@ -1,4 +1,12 @@
 package Oppgaver;
 
 public class Oppgave1 {
+
+    public static void main(String[] args){
+
+        for (int j = 0; j < 10; j++){
+            System.out.println(j);
+        }
+
+    }
 }
