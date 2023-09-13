3900b328e8639c5a44e8316da75007a1c1255d92
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-09-13 13:12:43 +0200

    oppgave2

--- Arrays/src/Oppgaver/Oppgave1.java
+++ Arrays/src/Oppgaver/Oppgave1.java
@@ -1,4 +1,24 @@
 package Oppgaver;
 
 public class Oppgave1 {
-}
+    public static void main(String[] args){
+        int liste [] = new int[9];
+                liste [0] = 13;
+                liste [1] = 15;
+                liste [2] = 18;
+                liste [3] = 31;
+                liste [4] = 21;
+                liste [5] = 22;
+                liste [6] = 32;
+                liste [7] = 1793;
+                liste [8] = 849582;
+
+                String ut = "Her kommer tallene: ";
+                ut += liste[3] + " ";
+                ut += liste[6] + " ";
+
+                System.out.print(ut);
+
+        }
+    }
+
