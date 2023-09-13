c4e0af1f3abb2908de3283cf763c3cc30c8219bf
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-09-13 16:43:36 +0200

    oppgave7

--- Arrays/src/Oppgaver/Oppgave6.java
+++ Arrays/src/Oppgaver/Oppgave6.java
@@ -1,7 +1,17 @@
 package Oppgaver;
 
-public class Oppgave7 {
+import java.util.Arrays;
+
+public class Oppgave6 {
     public static void main(String[] args){
-        
+        int liste [] = {2,5,6,7,8};
+        int liste2 [] = {2,5,6,7,8};
+        if (Arrays.equals(liste, liste2)){
+            System.out.println("Arrayne er like");
+        }
+        else { System.out.println("Arrayne er ikke like");}
+
+
+        }
     }
-}
+
---
+++ Arrays/src/Oppgaver/Oppgave7.java
@@ -0,0 +1,7 @@
+package Oppgaver;
+
+public class Oppgave7 {
+    public static void main(String[] args){
+        
+    }
+}
