840817db3d1151c0e9dc355f14462b86538c0062
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-09-13 16:35:34 +0200

    oppgave 6

--- Arrays/src/Oppgaver/Oppgave5.java
+++ Arrays/src/Oppgaver/Oppgave5.java
@@ -1,7 +1,33 @@
 package Oppgaver;
 
 public class Oppgave5 {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+        int liste[] = {45, 34, 5, 8, 10, 2, 13, 25};
+        int minsteTall = min(liste);
+        int makstall = max(liste);
+        System.out.println("Minste tall " + minsteTall);
+        System.out.println("Største tall " + makstall);
+    }
+
+    public static int min(int[] liste) {
+        int minste = liste[0];
+        for (int tall : liste) {
+            if (tall < minste) {
+                minste = tall;
+            }
+        }
+        return minste;
+
+    }
+
+    public static int max(int[] liste) {
+        int maks = liste[0];
+        for (int tall : liste) {
+            if (tall > maks) {
+                maks = tall;
+            }
+        }
+        return maks;
+
     }
 }
