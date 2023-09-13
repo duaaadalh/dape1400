72dc35ef5f84d634cb93d0e5070876b2dc5ef37f
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-09-13 20:11:47 +0200

    oppgave10

--- Arrays/src/Oppgaver/Oppgave7.java
+++ Arrays/src/Oppgaver/Oppgave7.java
@@ -2,6 +2,22 @@ package Oppgaver;
 
 public class Oppgave7 {
     public static void main(String[] args){
-        
+        int liste [] = {1,2,5,6,8};
+        int antallPartall = 0;
+        int antallOddetall = 0;
+
+        for (int i = 0; i < liste.length; i++){
+            if (liste[i] % 2 == 0){
+                antallPartall ++;
+            }
+            else {
+                antallOddetall ++;
+            }
+
+        }
+        System.out.println("Antall partall er: " + antallPartall);
+        System.out.println("Antall oddetall er: " + antallOddetall);
+
+
     }
 }
--- Kontrollstruktorer2/src/Oppgaver/Oppgave9.java
+++ Kontrollstruktorer2/src/Oppgaver/Oppgave9.java
@@ -6,13 +6,28 @@ public class Oppgave9 {
     public static void main(String[] args){
        double pi = 0;
        double s = 1;
-       int n = 10000;
+       int n = 100000;
        for (int i = 1; i < n; i = i+2){
            pi += s/i;
            s = -s;
 
        }
        System.out.println("Pi = " + pi * 4);
+
+       // Pi=4       -4/3     +4/5-4/7+4/9-4/11+....
+        // 0 + 4 / 1 (-4 / 3)
+
+
+
+        double pie = 0;
+        double plussminus = 4;
+        int antallrunder = 5000;
+        for (int u = 0; u < antallrunder; u++){
+            pie += plussminus / (u*2 + 1) ;
+
+            plussminus = -plussminus;
+        }
+        System.out.println("Pi = " + pie);
 
 
 
