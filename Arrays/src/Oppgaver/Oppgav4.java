7cbfef45dbdc2f8001a76894d87d381672c7de1f
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-09-13 16:13:18 +0200

    oppgave5

--- Arrays/src/Oppgaver/Oppgav4.java
+++ Arrays/src/Oppgaver/Oppgav4.java
@@ -2,6 +2,24 @@ package Oppgaver;
 
 public class Oppgav4 {
     public static void main(String[] args){
-        
+        int liste [] = {45,34,5,8,10,2,13,25};
+
+        for (int i = liste.length - 1; i>=0; i--){
+            System.out.println(liste[i]);
+
+
+
+        }
+
     }
 }
+
+// for (int i = 0; i < liste.length; i -- ){
+//   System.out.println(liste[i]);
+// det over er da feil
+
+
+//for (int i = aList.size() - 1; i >= 0; i--) {
+            //String s = aList.get(i);
+           // }
+// 45, 34, 5, 8, 10, 2, 13, 25
