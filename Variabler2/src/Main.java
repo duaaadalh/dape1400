dd1055a3fce5c5ce9a3a4d4af7aacea6688aa029
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-09-18 20:37:14 +0200

    slettet mappe

--- Variabler2/src/Main.java
+++ Variabler2/src/Main.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 import static javax.swing.JOptionPane.showInputDialog;
 import static javax.swing.JOptionPane.showMessageDialog;
 
@@ -11,6 +13,26 @@ public class Main {
         String innForAlder = showInputDialog("Min alder er ");
         alder = Integer.parseInt(innForAlder); // konvertere string til int
         showMessageDialog(null,"Min alder er "+ innForAlder + " år");
+
+        // oppgave2
+
+        double tall1;
+        double tall2;
+        double tall3;
+
+        tall1 = 3.67;
+        tall2 = 8.34;
+        tall3 = 92.64;
+
+        double sumAvAlleTall = tall1+tall2+tall3;
+
+        System.out.println(sumAvAlleTall);
+
+        // oppgave3
+
+        double gjennomsnitt = sumAvAlleTall/3;
+        System.out.println("Gjennomsnittet av tallen er " + sumAvAlleTall);
+        JOptionPane.showMessageDialog(null, "Gjennomsnittet av tallene er" + gjennomsnitt);
 
 
     }
