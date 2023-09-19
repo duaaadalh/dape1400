c51d33fa2ce0599704cf02887388abbb537c8371
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-09-19 11:28:28 +0200

    klasser og objekter

--- Oppgave/src/main/java/com/example/oppgave/HelloController.java
+++ Oppgave/src/main/java/com/example/oppgave/HelloController.java
@@ -1,14 +1,40 @@
 package com.example.oppgave;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField txtAntallÅr;
+
+    @FXML
+    private TextField txtBeløp;
+
+    @FXML
+    private TextField txtRente;
+
+    @FXML
+    private Label lblResultat;
+
+    @FXML
+    void Beregn(ActionEvent event) {
+        try {
+            double beløp = Double.parseDouble(txtBeløp.getText());
+            double rente = Double.parseDouble(txtRente.getText());
+            int antallÅr = Integer.parseInt(txtAntallÅr.getText());
+            double resultat = beløp * Math.pow(1+(rente/100),antallÅr);
+            lblResultat.setText("Totalbeløpet etter "+antallÅr+" år blir "+String.format("%.2f",resultat));
+
+        }
+        catch (Exception e){
+            lblResultat.setText("Skriv inn tall i alle feltene!");
+        }
+
+        }
     }
-}
+
+
+
