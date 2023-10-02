0796051aa8fb336fcef531eabcb7d96066871bd2
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-10-02 08:53:52 +0200

    Endringer

--- Oblig1/src/Oppgave1/Sirkel.java
+++ Oblig1/src/Oppgave1/Sirkel.java
@@ -1,4 +1,31 @@
 package Oppgave1;
 
 public class Sirkel {
+
+
+    public static double diameter (double radius){
+        return radius*2;
+    }
+
+    public static double omkrets (double radius){
+        return 2*Math.PI * radius;
+    }
+
+    public static double areal (double radius){
+        return Math.PI*Math.pow(radius,2);
+    }
+
+    
+
+    public static void main(String[] args) {
+        double radius = 6.7;
+        double omkretsSirkel = omkrets(radius);
+        double diameterSirkel = diameter(radius);
+        double arealSirkel = areal(radius);
+
+        System.out.println("Arealet er " + arealSirkel);
+
+    }
 }
+
+
