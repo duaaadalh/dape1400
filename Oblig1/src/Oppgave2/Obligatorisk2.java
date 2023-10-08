64794ac2884fd1e5ea8d053546e01ab8eba6ad54
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-10-08 10:36:30 +0200

    ramile

--- Oblig1/src/Oppgave2/Obligatorisk2.java
+++ Oblig1/src/Oppgave2/Obligatorisk2.java
@@ -3,37 +3,33 @@ package Oppgave2;
 import javax.swing.*;
 
 public class Obligatorisk2 {
+    public static void main(String[] args) {
 
+        // bruk avvikshåntering (try/catch) for å konvertere
 
-        public static void main(String[] args) {
+        try {
+            // les inn navn, antall og pris for vare 1
+            String navn1 = JOptionPane.showInputDialog("Skrive navn til vare1 ");
+            int antall1 = Integer.parseInt(JOptionPane.showInputDialog("Skrive antall av vare1: "));
+            double pris1 = Double.parseDouble(JOptionPane.showInputDialog("Skrive pris av vare1: "));
 
-            // bruk avvikshåntering (try/catch) for å konvertere
+            // les inn navn, antall og pris for vare 2
+            String navn2 = JOptionPane.showInputDialog("Skrive navn til vare2 ");
+            int antall2 = Integer.parseInt(JOptionPane.showInputDialog("Skrive antall av vare2: "));
+            double pris2 = Double.parseDouble(JOptionPane.showInputDialog("Skrive pris av vare2: "));
 
-            try {
-                // les inn navn, antall og pris for vare 1
-                String navn1 = JOptionPane.showInputDialog("Skrive navn til vare1 ");
-                int antall1 = Integer.parseInt(JOptionPane.showInputDialog("Skrive antall av vare1: "));
-                double pris1 = Double.parseDouble(JOptionPane.showInputDialog("Skrive pris av vare1: "));
+            // opprett to vare-objekter med de innleste verdiene
+            Vare vare1 = new Vare(navn1, antall1, pris1);
+            Vare vare2 = new Vare(navn2, antall2, pris2);
 
-                // les inn navn, antall og pris for vare 2
-                String navn2 = JOptionPane.showInputDialog("Skrive navn til vare2 ");
-                int antall2 = Integer.parseInt(JOptionPane.showInputDialog("Skrive antall av vare2: "));
-                double pris2 = Double.parseDouble(JOptionPane.showInputDialog("Skrive pris av vare2: "));
+            // beregn total pris for alle varer i System.out
+            double totalPris = vare1.totalPrisen() + vare2.totalPrisen();
 
-                // opprett to vare-objekter med de innleste verdiene
-                Vare vare1 = new Vare(navn1, antall1, pris1);
-                Vare vare2 = new Vare(navn2, antall2, pris2);
+            // skriv ut totalprisen
+            System.out.println(String.format("Totalprisen ble : %s kr.", totalPris));
+        } catch (NumberFormatException e) {
+            System.out.println("Feil format for antall eller pris.");
 
-                // beregn total pris for alle varer i System.out
-                double totalPris = vare1.totalPrisen() + vare2.totalPrisen();
-
-                // skriv ut totalprisen
-                System.out.println(String.format("Totalprisen ble : %s kr.", totalPris));
-            } catch (NumberFormatException e) {
-                System.out.println("Feil format for antall eller pris.");
-
-            }
         }
     }
-
-
+}
--- Oblig1/src/Oppgave2/Vare.java
+++ Oblig1/src/Oppgave2/Vare.java
@@ -1,60 +1,54 @@
 package Oppgave2;
 
-public class Vare {
-    public class Vare {
-
-        private String navn;
-        private int antall;
-        private double pris;
+public class Vare {private String navn;
+    private int antall;
+    private double pris;
 
 
-        // Konstruktør med alle attributter
-        public Vare(String navn, int antall, double pris) {
-            this.navn = navn;
-            this.antall = antall;
-            this.pris = pris;
-        }
-
-        // get / set metoder
-        // antall og pris må være større enn null
-
-        public String getNavn() {
-            return navn;
-        }
-
-        public void setNavn(String navn) {
-            this.navn = navn;
-        }
-
-        public int getAntall() {
-            return antall;
-        }
-
-        public void setAntall(int antall) {
-            if (antall > 0) {
-                this.antall = antall;
-            } else {
-                System.out.println("Antall må være større enn null.");
-            }
-        }
-
-        public double getPris() {
-            return pris;
-        }
-
-        public void setPris(double pris) {
-            if (pris > 0) {
-                this.pris = pris;
-            } else {
-                System.out.println("Prisen må være større enn null.");
-            }
-        }
-
-        // metode for å beregne totalpris (pris * antall)
-        public double totalPrisen() {
-            return pris * antall;
-        }
-
+    // Konstruktør med alle attributter
+    public Vare(String navn, int antall, double pris) {
+        this.navn = navn;
+        this.antall = antall;
+        this.pris = pris;
     }
 
+    // get / set metoder
+    // antall og pris må være større enn null
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+
+    public int getAntall() {
+        return antall;
+    }
+
+    public void setAntall(int antall) {
+        if (antall > 0) {
+            this.antall = antall;
+        } else {
+            System.out.println("Antall må være større enn null.");
+        }
+    }
+
+    public double getPris() {
+        return pris;
+    }
+
+    public void setPris(double pris) {
+        if (pris > 0) {
+            this.pris = pris;
+        } else {
+            System.out.println("Prisen må være større enn null.");
+        }
+    }
+
+    // metode for å beregne totalpris (pris * antall)
+    public double totalPrisen() {
+        return pris * antall;
+    }
 }
