0cee42370871b312d611f4349587a07a69c492a3
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-10-09 11:17:20 +0200

    modul2

--- Repetisjon/src/Modul1/Oppgave1.java
+++ Repetisjon/src/Modul1/Oppgave1.java
@@ -7,9 +7,26 @@ public class Oppgave1 {
 
         String fornavn = JOptionPane.showInputDialog("Skriv inn fornavnet");
         String etternavn = JOptionPane.showInputDialog("Skriv inn etternavnet");
+        String alder = JOptionPane.showInputDialog("Skriv inn alderen din");
+        String adresse = JOptionPane.showInputDialog("Skriv inn adresse");
+        String poststed = JOptionPane.showInputDialog("Skriv inn poststed");
+        String postnummer = JOptionPane.showInputDialog("Skriv inn postnummer");
         String navn = fornavn + " " + etternavn;
-        JOptionPane.showMessageDialog(null, navn);
 
+      //  JOptionPane.showMessageDialog(null, navn);
+
+       // JOptionPane.showMessageDialog(null, "Alderen til " + navn + " er " + alder + " år");
+
+
+       // JOptionPane.showMessageDialog(null, navn + " bor i " + adresse + " som har postnummer " +postnummer
+      //  + " i " + poststed + ". " + fornavn + "'s alder er " + alder + " år");
+
+      //  JOptionPane.showMessageDialog(null, "Navn: " + navn + "\n" + "Adresse: " + adresse +"\n"
+      //  + "Postnummer: " + postnummer + "\n" + "Poststed: " + poststed + "\n" + "Alder: " + alder + " år.");
+
+        System.out.println(navn + " bor i " + adresse + " som har postnummer " + postnummer + " i "+ poststed + ". " + fornavn
+        + "'s alder er " + alder + " år.");
     }
+
 }
-}
+
