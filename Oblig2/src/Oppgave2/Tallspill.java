2322a5b5318a42e255083fc68250962e4f3548c6
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-10-11 11:19:07 +0200

    modul4

--- Oblig2/src/Oppgave2/Tallspill.java
+++ Oblig2/src/Oppgave2/Tallspill.java
@@ -1,4 +1,42 @@
 package Oppgave2;
 
+import javax.swing.*;
+
 public class Tallspill {
+
+
+        private int slumptall;
+        private int antallGjetninger;
+
+
+        public int nyttTall () {
+            slumptall = (int) (Math.random() * 201);
+            return slumptall;
+        }
+
+        public void visMelding (String melding){
+            JOptionPane.showMessageDialog(null, melding);
+        }
+
+        public void forLite (int tall){
+            visMelding(tall + "Tallet er for lite. Prøv igjen");
+        }
+        public void forStort (int tall){
+            visMelding(tall + "Talle er for høyt. Prøv igjen");
+        }
+
+        public void avsluttRunde (int antall, int gjetning){
+            visMelding("Du har gjettet det riktige tallet " + gjetning + ". Etter " + antall + " forsøk");
+        }
+
+
+        public void kjørSpill(){
+            nyttTall();
+            antallGjetninger = 0;
+
+        }
+
+    public static void main (String [] args) {}
+
+
 }
