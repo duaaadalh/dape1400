c64e8f8d78ec9fff2f99c4bb30d29a795598e6aa
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-10-16 15:18:38 +0200

    oppgave2

--- Repetisjon/src/Modul6/Oppgave1.java
+++ Repetisjon/src/Modul6/Oppgave1.java
@@ -1,4 +1,23 @@
 package Modul6;
 
+import javax.swing.*;
+
 public class Oppgave1 {
+
+    public static void main (String [] args){
+
+        String innAlder = JOptionPane.showInputDialog("Skriv inn alderen din");
+        int alder = Integer.parseInt(innAlder);
+
+        if (alder >= 18 && alder <= 20){
+            JOptionPane.showMessageDialog(null, "Du er myndig, men kan ikke kjøpe alkohol for mer enn 22%");
+
+
+        }
+
+            else {
+                JOptionPane.showMessageDialog(null, "Du er enten umynding, eller så kan du kjøpe hva du vil på polet");
+        }
+
+    }
 }
