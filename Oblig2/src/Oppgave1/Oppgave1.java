d494ea49629458c07df415efbc1d067a0985dd78
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-10-16 15:12:21 +0200

    modul6

--- Oblig2/src/Oppgave1/Oppgave1.java
+++ Oblig2/src/Oppgave1/Oppgave1.java
@@ -1,5 +1,47 @@
 package Oppgave1;
+import javax.swing.*;
+
 
 public class Oppgave1 {
 
+
+    public static void main (String [] args){
+
+        int sum = 0;
+        String sumtekst = "";
+        int nedre, ovre;
+
+        do {
+            String nedreGrensen = JOptionPane.showInputDialog("Skrive inn nedre grensen: ");
+            nedre = Integer.parseInt(nedreGrensen);
+
+            String ovesteGrensen = JOptionPane.showInputDialog("Skrive inn øvre grensen: ");
+            ovre = Integer.parseInt(ovesteGrensen);
+
+            //Dersom innlest øvre grense er mindre enn eller lik nedre grense,
+            // skal programmet skrive ut en melding til brukeren om dette og foreta ny innlesing.
+            if (ovre <= nedre) {
+                JOptionPane.showMessageDialog(null, "Nedre grense må være mindre enn øvre grense.");
+            }
+        }
+        while ( ovre <= nedre);
+
+        for (int i = nedre; i < ovre; i++) {
+            sum += i;
+            sumtekst += i;
+            // Legg til " + " etter tallet, med linjeskift for hvert 10. tall
+            if (i != ovre) {
+                sumtekst += " + ";
+                if (i % 10 == 0) {
+                    sumtekst += "\n";
+                }
+            }
+        }
+        System.out.println(sumtekst+ " = " + sum);
+        JOptionPane.showMessageDialog(null, nedre + " + " + (nedre + 1) + " + ... + " + ovre + " = " + sum);
+    }
 }
+
+
+
+
--- Oblig2/src/Oppgave2/Tallspill.java
+++ Oblig2/src/Oppgave2/Tallspill.java
@@ -2,22 +2,23 @@ package Oppgave2;
 
 import javax.swing.*;
 
+
 public class Tallspill {
 
 
         private int slumptall;
         private int antallGjetninger;
 
-
+        // Her trekkes det og returneres et slumptall mellom 0 og 200
         public int nyttTall () {
             slumptall = (int) (Math.random() * 201);
             return slumptall;
         }
-
+        // Definerer visMelding metode
         public void visMelding (String melding){
             JOptionPane.showMessageDialog(null, melding);
         }
-
+        // Metode som forteller brukeren at tallet de skrev inn er for lite eller for stort
         public void forLite (int tall){
             visMelding(tall + "Tallet er for lite. Prøv igjen");
         }
@@ -25,6 +26,7 @@ public class Tallspill {
             visMelding(tall + "Talle er for høyt. Prøv igjen");
         }
 
+        //Metode hvor brukeren har gjettet riktig og får opp hvor mange forsøk de har brukt
         public void avsluttRunde (int antall, int gjetning){
             visMelding("Du har gjettet det riktige tallet " + gjetning + ". Etter " + antall + " forsøk");
         }
@@ -36,6 +38,7 @@ public class Tallspill {
             int tallGjettet;
             boolean gjettetRiktig = false;
 
+            //While-løkke for å bruke spillet. Hvor programmet går gjennom tallene som brukeren gjetter
             while (!gjettetRiktig){
                 try {
                     tallGjettet = Integer.parseInt(JOptionPane.showInputDialog("Gjett et tall mellom 0-200"));
@@ -52,7 +55,8 @@ public class Tallspill {
                         avsluttRunde(antallGjetninger, tallGjettet);
 
                     }
-
+                    // Bruker catch for å sikre at brukeren får opp en feilmelding ved inntast av
+                    //ugyldig tall
                 } catch (Exception e){
                     visMelding("Skriv inn et gyldig tall");
                 }
