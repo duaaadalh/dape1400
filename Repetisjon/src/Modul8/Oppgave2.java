e7eabed48d9e3e09170467b8071474927176d4c0
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-10-17 11:38:04 +0200

    oppgave3

--- Repetisjon/src/Modul8/Oppgave2.java
+++ Repetisjon/src/Modul8/Oppgave2.java
@@ -1,4 +1,29 @@
 package Modul8;
 
 public class Oppgave2 {
+    public static void main (String [] args){
+        int [] liste = {1,2,3,4,5,6,7,8,9};
+        int sum = 0;
+        for (int i = 0; i< liste.length; i++){
+            sum += liste.length;
+        }
+        System.out.println(sum);
+        double gjennomsnitt = (double)sum/liste.length;
+        System.out.println(gjennomsnitt);
+
+        sum = 0;
+        int teller = 0;
+        for (int i : liste){
+            if (i>5){
+                sum +=i;
+                teller++;
+            }
+
+
+        }
+        System.out.println(sum);
+        gjennomsnitt = (double)sum/teller;
+        System.out.println(gjennomsnitt);
+
+    }
 }
