ece731fe94d7f1a8834b6b875f966c1fab78b5b2
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-10-17 09:44:56 +0200

    modul7

--- Repetisjon/src/Modul6/Oppgave6.java
+++ Repetisjon/src/Modul6/Oppgave6.java
@@ -1,4 +1,33 @@
 package Modul6;
 
+import javax.swing.*;
+
 public class Oppgave6 {
+    public static void main(String[] args){
+        String måned = JOptionPane.showInputDialog("Skriv inn en måned");
+        switch (måned){
+            case "januar":
+            case "februar":
+            case "desember":
+                JOptionPane.showMessageDialog(null, "Det er vinter");
+                break;
+
+            case "mars":
+            case "april":
+            case "mai":
+                JOptionPane.showMessageDialog(null, "Det er vår");
+
+            case "juni":
+            case "juli":
+            case "august":
+                JOptionPane.showMessageDialog(null, "Det er sommer");
+
+            case "september":
+            case "oktober":
+            case "november":
+                JOptionPane.showMessageDialog(null, "Det er høst");
+                break;
+            default:JOptionPane.showMessageDialog(null, "Ugyldig måned");
+        }
+    }
 }
---
+++ Repetisjon/src/Modul7/Oppgaver.java
@@ -0,0 +1,4 @@
+package Modul7;
+
+public class Oppgaver {
+}
