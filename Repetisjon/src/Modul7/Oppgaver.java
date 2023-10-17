747d6e13d8b28cd652f337fe900bb2c369e17d53
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-10-17 11:18:32 +0200

    modul8

--- Repetisjon/src/Modul7/Oppgaver.java
+++ Repetisjon/src/Modul7/Oppgaver.java
@@ -1,4 +1,91 @@
 package Modul7;
 
 public class Oppgaver {
-}
+    public static void main (String [] args){
+
+
+            //oppgave 1
+      //  for (int i = 0; i < 10; i++ ) {
+       //     System.out.println(i);
+
+            //oppgave2
+
+       // int sum = 0;
+       // for (int i = 0; i < 10; i++){
+       //     sum += i;
+
+        //}
+       // System.out.println(sum);
+
+        //oppgave3
+
+       // for (int i = 0; i < 20; i+=2){
+         //   System.out.print(i + " ");
+       // }
+
+        //oppgave4
+
+       // int sum = 0;
+       // int teller = 0;
+       // for (int i = 0; i < 100; i+=2){
+       //     sum +=i;
+        //    teller ++;
+       // }
+       // System.out.println(sum);
+       // double gjennomsnitt = (double) sum / teller;
+       // System.out.println(gjennomsnitt);
+
+
+        //oppgave5
+
+       // int i = 0;
+       // while (i < 10){
+          //  System.out.println(i);
+         //   i++;
+        //}
+
+        //Oppgave6
+
+      //  int sum = 0;
+       // int i = 0;
+       // while (i < 10){
+         //   sum += i;
+         //   i++;
+
+
+      //  }
+        //System.out.println(sum);
+
+        //Oppgave7
+
+       // int i = 0;
+       // while (i <20){
+        //    System.out.println(i);
+        //    i+=2;
+      //  }
+
+
+        //Oppgave8
+
+        int i = 0;
+        int sum = 0;
+        int teller = 0;
+        while (i < 100){
+            sum +=i;
+            i +=2;
+            teller++;
+
+        }
+        System.out.println(sum);
+        double gjennomsnitt = (double) sum/teller;
+        System.out.println(gjennomsnitt);
+
+        }
+        }
+
+
+
+
+
+
+
