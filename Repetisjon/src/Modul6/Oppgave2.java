67d8ebc294185c4874094cf3b4c8b482767c4fea
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-10-17 08:52:56 +0200

    oppgave5

--- Repetisjon/src/Modul6/Oppgave2.java
+++ Repetisjon/src/Modul6/Oppgave2.java
@@ -1,4 +1,36 @@
 package Modul6;
 
+import javax.swing.*;
+
 public class Oppgave2 {
+
+    public static void main (String [] args){
+        String innAlder = JOptionPane.showInputDialog("Skriv inn alderen din");
+        int alder = Integer.parseInt(innAlder);
+
+        if (alder < 18){
+            JOptionPane.showMessageDialog(null, "Du er for ung til å ta førerkort");
+
+
+        }
+
+        else if (alder < 70){
+            JOptionPane.showMessageDialog(null, "Du kan ta førerkort og trenger ikke legesjekk");
+        }
+
+        else {
+            JOptionPane.showMessageDialog(null, "Du er absolutt gammel nok, men må ta årlig legesjekk");
+        }
+
+
+       // if (alder <= 18){
+         //   JOptionPane.showMessageDialog(null, "Du er for ung til å ta førerkort for bil");
+
+        //else {
+            //JOptionPane.showMessageDialog(null, "Du er for ung til å ta førerkort");
+
+
+
+
+    }
 }
