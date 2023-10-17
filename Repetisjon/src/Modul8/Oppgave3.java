3035cf5e52c4d6b784ab7f08cc3d9aeb999ffc0e
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-10-17 11:42:54 +0200

    oppgave4

--- Repetisjon/src/Modul8/Oppgave3.java
+++ Repetisjon/src/Modul8/Oppgave3.java
@@ -1,4 +1,11 @@
 package Modul8;
 
 public class Oppgave3 {
+    public static void main (String [] args){
+        String [] navn = {"Per", "Hansen", "Line" , "Olsen", "Ola" , "Pedersen",
+        "Kari", "Finsen"};
+        for (int i = 0; i< navn.length; i+=2){
+            System.out.println(navn[i] + " " +navn[i+1]);
+        }
+    }
 }
