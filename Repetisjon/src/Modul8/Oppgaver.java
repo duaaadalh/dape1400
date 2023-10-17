19f1024427f532bbcd26345e77a96ae3dad1ec36
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-10-17 11:28:59 +0200

    oppgave2

--- Repetisjon/src/Modul8/Oppgaver.java
+++ Repetisjon/src/Modul8/Oppgaver.java
@@ -1,4 +1,27 @@
 package Modul8;
 
 public class Oppgaver {
+    public static void main (String [] args){
+        int [] liste = {1,2,3,4,5,6,7,8,9};
+
+        //eller
+
+        int [] liste1 = new int[9];
+        liste1[0] = 1;
+        liste1[1] = 2;
+        liste1[2] = 3;
+        liste1[3] = 4;
+        liste1[4] = 5;
+        liste1[5] = 6;
+        liste1[6] = 7;
+        liste1[7] = 8;
+        liste1[8] = 9;
+
+
+      System.out.println("Posisjonen er " +liste[3] + " og "+ liste[6]);
+
+
+
+
+    }
 }
