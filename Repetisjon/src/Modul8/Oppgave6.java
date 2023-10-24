cf6355c2408511d2b2a4f8afd14b3cd6fa47b775
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-10-24 09:26:53 +0200

    oblig3

--- Repetisjon/src/Modul8/Oppgave6.java
+++ Repetisjon/src/Modul8/Oppgave6.java
@@ -1,4 +1,28 @@
 package Modul8;
 
 public class Oppgave6 {
+    public static void main (String [] args){
+
+        int [] liste = {1,2,3,4,5,6,7,8};
+        int [] liste1 = {1,2,3,4,5,6,7,8};
+
+        if (liste1.length != liste.length){
+            System.out.println("De er ikke like lange og dermed ikke like");
+        }
+        else {
+            boolean like = true;
+            for (int i = 0; i < liste1.length; i++){
+                if (liste1[i] != liste[i]){
+                    like = false;
+                }
+            }
+            if (like){
+                System.out.println("De er like");
+            }
+            else {
+                System.out.println("De er ikke like");
+            }
+        }
+
+    }
 }
