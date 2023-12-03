5ed09bdae1772c016816f7744a90e65911fddd2f
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-12-03 15:18:41 +0100

    repetisjon

--- Eksamen/src/Eksamen2022/Oppgave3.java
+++ Eksamen/src/Eksamen2022/Oppgave3.java
@@ -1,7 +1,42 @@
 package Eksamen2022;
 
+import javax.swing.*;
+
+
 public class Oppgave3 {
     public static void main(String[] args){
-        
+
+
+        double vekt;
+        double høyde;
+        String innVekt = JOptionPane.showInputDialog("Skriv inn vekten i KG");
+        String innHøyde = JOptionPane.showInputDialog("Skriv inn høyden i CM");
+
+        try {
+            vekt = Double.parseDouble(innVekt);
+            høyde = Double.parseDouble(innHøyde);
+
+
+
+        }
+        catch (Exception e){
+            vekt = 0;
+            høyde = 0;
+
+        }
+
+        if (vekt <= 0 || høyde <= 0){
+            JOptionPane.showMessageDialog(null, "Du har skrevet inn ugyldige tall");
+        }
+        else {
+            double BMI = (1.3 * vekt)/(Math.pow(høyde/100,2.5));
+            JOptionPane.showMessageDialog(null, "Din BMI er " + String.format("%.2f", BMI));
+        }
+
+
+
+
+
+
     }
 }
---
+++ Eksamen/src/Eksamen2022/RepetisjonOppgave3.java
@@ -0,0 +1,8 @@
+package Eksamen2022;
+
+public class RepetisjonOppgave3 {
+    public static void main(String[] args){
+        
+    }
+
+}
