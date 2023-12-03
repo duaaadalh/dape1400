11a6b5e4b329a6d7a928f6030639948e6b6fe6c3
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-12-03 16:01:03 +0100

    repetisjon igjen

---
+++ Eksamen/src/Eksamen2022/RepOppgave2.java
@@ -0,0 +1,26 @@
+package Eksamen2022;
+
+
+class Liste3{
+
+
+    public static int positiveTall (int [] liste){
+        int teller = 0;
+        for (int i : liste){
+            if (i>0){
+                teller++;
+            }
+        }
+        return teller;
+    }
+
+    publi
+
+}
+
+public class RepOppgave2 {
+    public static void main(String[] args){
+
+    }
+
+}
--- Eksamen/src/Eksamen2022/RepetisjonOppgave4.java
+++ Eksamen/src/Eksamen2022/RepetisjonOppgave4.java
@@ -1,7 +1,25 @@
 package Eksamen2022;
 
+
+class KonvertererTemperaturer{
+
+    public static double tilCelsius (double innFarenheit){
+        return (innFarenheit - 32)*1.8;
+    }
+    
+    public static double tilFarenheit (double innCelsius){
+        return (innCelsius*1.8)+32;
+    }
+
+}
+
 public class RepetisjonOppgave4 {
     public static void main(String[] args){
-        
+    double celsius = 35.8;
+    System.out.println(celsius + " celsius blir " + KonvertererTemperaturer.tilFarenheit(celsius) + " farenheit");
+    double farenheit = 88.3;
+    System.out.println(farenheit + " farenheit blir " + String.format("%.2f" , KonvertererTemperaturer.tilCelsius(farenheit))+ " celsius");
+
+
     }
 }
