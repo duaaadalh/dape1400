01f550f8f84816aea9bda87cbc2aa3c665ab75e6
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-12-04 13:18:09 +0100

    rep

--- Eksamen/src/Eksamen2022/reOpg2.java
+++ Eksamen/src/Eksamen2022/reOpg2.java
@@ -1,7 +1,49 @@
 package Eksamen2022;
 
+
+class Lister{
+
+    public static int merEnnNull (int [] liste){
+        int teller = 0;
+        for (int i : liste){
+            if (i > 0){
+                teller++;
+            }
+        }
+        return teller;
+    }
+
+    public static int forekomsten (int [] liste, int tall){
+        int teller = 0;
+        for (int i : liste){
+            if (i == tall){
+                teller++;
+            }
+        }
+        return teller;
+    }
+
+    public static int storst (int [] liste){
+        int makss = liste[0];
+        for (int i : liste){
+            if (i > makss){
+                makss =  i;
+            }
+        }
+        return makss;
+    }
+
+}
+
 public class reOpg2 {
     public static void main(String[] args){
-        
+        int liste [] = {1,1,2,4,5,-7,-90654,11, 1, 5};
+
+        System.out.println("Antall tall større enn 10 er " + Lister.merEnnNull(liste));
+        System.out.println("Tallet 1 forekommer " + Lister.forekomsten(liste, 1));
+        System.out.println("Det største tallet er " + Lister.storst(liste));
+
+
+
     }
 }
---
+++ Eksamen/src/Eksamen2022/reOpg3.java
@@ -0,0 +1,7 @@
+package Eksamen2022;
+
+public class reOpg3 {
+    public static void main(String[] args){
+        
+    }
+}
