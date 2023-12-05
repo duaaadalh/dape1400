ee9864885994e0775f36a7592054e7cd25201af8
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-12-05 10:48:33 +0100

    oppgave2Rep

--- RepetisjonIgjen/src/Oppgave1.java
+++ RepetisjonIgjen/src/Oppgave1.java
@@ -1,7 +1,7 @@
 public class Oppgave1 {
 
     public static void main(String[] args) {
-        int list [] = {-3, 45, 5, -3, 2, 49};
+        int list [] = {-3, 41, 5, -3, 2, 49};
 
         int teller = 0;
         for (int i : list){
@@ -11,6 +11,25 @@ public class Oppgave1 {
         }
 
         System.out.println(teller);
+
+
+        for (int i = 0; i < list.length; i +=2){
+            System.out.println(list[i]);
+        }
+
+        int positiv = 0;
+        int sum = 0;
+        for (int i : list){
+            if (i>0){
+                sum += i;
+                positiv++;
+            }
+        }
+
+        System.out.println(sum);
+
+        double gjennomsnitt = (double) sum / positiv;
+        System.out.println(gjennomsnitt);
 
     }
 
