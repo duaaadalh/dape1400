9b63954a0d3de5c654cd6f0689552f8585807564
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-12-05 10:41:54 +0100

    hei

--- RepetisjonIgjen/src/Oppgave1.java
+++ RepetisjonIgjen/src/Oppgave1.java
@@ -1,4 +1,20 @@
 public class Oppgave1 {
 
-    
+    public static void main(String[] args) {
+        int list [] = {-3, 45, 5, -3, 2, 49};
+
+        int teller = 0;
+        for (int i : list){
+            if (i > 10){
+                teller++;
+            }
+        }
+
+        System.out.println(teller);
+
+    }
+
+
+
+
 }
