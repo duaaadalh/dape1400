7a558da070cebeead8c8fbe4ccca06b1c372430b
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-12-06 15:18:13 +0100

    oppgave2

--- OvingTilEksamen/src/Oblig1.java
+++ OvingTilEksamen/src/Oblig1.java
@@ -1,2 +1,28 @@
+
+class Sirkel{
+    public static double beregnDiameter (double radius){
+        double diameter = radius*2;
+        return (diameter);
+    }
+
+    public static double beregnOmkrets (double radius){
+        double omkrets = (2*3.14*radius);
+        return (omkrets);
+    }
+
+    //Kan enten definere en double omkrets/areal/diameter også trykke return, eller ikke
+
+    public static double beregnAreal (double radius){
+        return (2*3.14*Math.pow(radius,2));
+    }
+}
+
 public class Oblig1 {
+    public static void main(String[] args) {
+        double radius = 34.5;
+        System.out.println("Arealet av en sirkel " + Sirkel.beregnAreal(radius));
+        System.out.println("Omkretsen av en sirkel er " + Sirkel.beregnOmkrets(radius));
+        System.out.println("Diameteren av en sirkel er " + Sirkel.beregnDiameter(radius));
+    }
+
 }
---
+++ OvingTilEksamen/src/Oblig1Opg2.java
@@ -0,0 +1,42 @@
+
+class Vare{
+    private String navn;
+    private int antall;
+    private double pris;
+
+    public Vare(String navn, int antall, double pris){
+        this.navn = navn;
+        this.antall = antall;
+        this.pris = pris;
+    }
+
+    public int getAntall() {
+        return antall;
+    }
+
+    public void setAntall(int antall) {
+        this.antall = antall;
+    }
+
+    public double getPris() {
+        return pris;
+    }
+
+    public void setPris(double pris) {
+        this.pris = pris;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+}
+
+public class Oblig1Opg2 {
+    public static void main(String[] args) {
+
+    }
+}
