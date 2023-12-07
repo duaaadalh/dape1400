6ced63a769c600daf0b445f241ca14b4bebad7ce
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-12-07 12:46:18 +0100

    oppgave2

--- Konte2021/src/Oppgave1.java
+++ Konte2021/src/Oppgave1.java
@@ -1,2 +1,43 @@
 public class Oppgave1 {
+
+    public static void main(String[] args) {
+
+        int liste [] = {20, 34, -4, 4, 5, 11, -23};
+
+        for (int i = 0; i < liste.length; i++){
+            System.out.print(liste[i] + " ");
+        }
+
+        for (int j = liste.length-1; j>=0; j--){
+            System.out.print(liste[j]+" ");
+
+
+        }
+        int sum = 0;
+        for (int k = 0; k<liste.length; k++){
+            sum += liste[k];
+
+        }
+        System.out.println(sum);
+
+
+        sum = 0;
+        int teller = 0;
+        for (int l = 0; l < liste.length; l++){
+            sum += liste[l];
+            teller++;
+        }
+
+        double gjennomsnitt = sum / (double) teller;
+        System.out.println(gjennomsnitt);
+
+
+
+        for (int m = 0; m < liste.length; m++){
+            if (liste[m] > 0 && liste[m] < 20){
+                System.out.println(liste[m]);
+
+            }
+        }
+    }
 }
