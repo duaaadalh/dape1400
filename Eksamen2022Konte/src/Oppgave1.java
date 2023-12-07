0f4fc4383aa20010ea3decded80800de90461578
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-12-07 11:21:48 +0100

    oppgave3

--- Eksamen2022Konte/src/Oppgave1.java
+++ Eksamen2022Konte/src/Oppgave1.java
@@ -4,28 +4,34 @@ public class Oppgave1 {
 
         int sum = 0;
 
-        for (int i = 0; i <= 1000; i+=10){
+        for (int i = 0; i <= 1000; i += 10) {
             sum += i;
 
-          System.out.println(sum);
+            System.out.println(sum);
         }
 
         sum = 0;
         int i = 0;
 
-        while (i <= 1000){
-            sum+=i;
-            i +=10;
+        while (i <= 1000) {
+            sum += i;
+            i += 10;
 
         }
 
         System.out.println(sum);
 
 
+        for (int k = 10; k >= -10; k--) {
+            if (k != -10) {
+                System.out.print(k + ",");
+            } else {
+
+                System.out.println(k);
 
 
-
-
-
+            }
+        }
     }
+
 }
--- Eksamen2022Konte/src/Oppgave2.java
+++ Eksamen2022Konte/src/Oppgave2.java
@@ -1,6 +1,47 @@
-public class Oppgave2 {
+import java.util.ArrayList;
 
+class Valuta{
+    private String valuta;
+    private double kurs;
+    public Valuta(String valuta, double kurs) {
+        this.valuta = valuta;
+        this.kurs = kurs;
+    }
+    public String getValuta() {
+        return valuta;
+    }
+    public double getKurs() {
+        return kurs;
+    }
+}
+
+class ValutaKalkulator {
+    private ArrayList<Valuta> valutakurser = new ArrayList<>();
+    public ValutaKalkulator(){
+       Valuta US = new Valuta("Dollar", 10);
+       valutakurser.add(US);
+       Valuta DK = new Valuta("Danske kroner", 8);
+       valutakurser.add(DK);
+
+    }
+    public double beregnKurs(String valuta) {
+
+        for (Valuta kurs : valutakurser){
+            if (kurs.getValuta().equals(valuta)){
+                return kurs.getKurs();
+            }
+        }
+    return 0;
+    }
+}
+
+
+
+
+public class Oppgave2 {
     public static void main(String[] args) {
-        
+        ValutaKalkulator k = new ValutaKalkulator();
+
+
     }
 }
