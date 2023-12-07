210a6ac19696444aba979a8265abf226bca914be
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-12-07 12:58:22 +0100

    oppgave3

--- Konte2021/src/Oppgave2.java
+++ Konte2021/src/Oppgave2.java
@@ -1,2 +1,31 @@
+import javax.swing.*;
+
 public class Oppgave2 {
+    public static void main(String[] args) {
+
+        String innVekt = JOptionPane.showInputDialog("Skriv inn vekten i kg");
+        String innHøyde = JOptionPane.showInputDialog("Skriv inn høyden i cm");
+
+        double vekt;
+        double høyde;
+
+        try {
+            vekt = Double.parseDouble(innVekt);
+            høyde = Double.parseDouble(innHøyde);
+        }
+
+        catch (Exception e){
+            vekt = 0;
+            høyde = 0;
+        }
+        if (vekt <= 0 || høyde <= 0){
+            JOptionPane.showMessageDialog(null, "Du har skrevet inn ugyldige tall");
+        }
+
+        else {
+            double BMI = (1.3 * vekt)/Math.pow(høyde/ 100,2.5);
+    JOptionPane.showMessageDialog(null, "Din BMI er " + String.format("%.2f", BMI));
+        }
+
+    }
 }
