a395f5e28c29346361097b1ca8d6edc46363dc18
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-12-08 18:42:35 +0100

    oppgave5

--- EksamenRepetisjon/src/Oppgave4.java
+++ EksamenRepetisjon/src/Oppgave4.java
@@ -1,6 +1,25 @@
+import javax.swing.*;
+
 public class Oppgave4 {
 
     public static void main(String[] args) {
-        
+
+        String CellerF = JOptionPane.showInputDialog("Skriv inn 'c' eller 'f'");
+        String gradeTallinn = JOptionPane.showInputDialog("Skriv inn tallet du vil ha i grader");
+        double grader = Double.parseDouble(gradeTallinn);
+
+        if (CellerF.equals("C")){
+            double celTilFar = (grader*1.8+32);
+            JOptionPane.showMessageDialog(null, "Antall celsius i farenheit blir " + celTilFar);
+        } else if (CellerF.equals("F")) {
+            double farTilCel = (grader-32)/1.8;
+            JOptionPane.showMessageDialog(null, "Antall farenheit i celsius blir " + farTilCel);
+
+        }
+        else {
+            JOptionPane.showMessageDialog(null, "Du har skrevet inn gyldige verdier");
+        }
+
+
     }
 }
