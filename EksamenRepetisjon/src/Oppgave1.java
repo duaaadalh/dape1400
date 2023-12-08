10960fa2c7111df50dbfe4066995cd63bc81edd2
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-12-08 15:18:01 +0100

    repetisjon

--- EksamenRepetisjon/src/Oppgave1.java
+++ EksamenRepetisjon/src/Oppgave1.java
@@ -1,2 +1,31 @@
 public class Oppgave1 {
+
+
+    public static void main(String[] args) {
+
+        int sum = 0;
+        int teller = 0;
+        for (int i = 5; i <=100; i+=5){
+            sum += i;
+            teller++;
+        }
+
+        System.out.println(sum);
+        double gjennomsnitt = sum / (double) teller;
+        System.out.println(gjennomsnitt);
+
+       int j = 5;
+       int sum1 = 0;
+       int teller1 = 0;
+       while (j <= 100){
+           sum1 += j;
+           teller1++;
+           j +=5;
+       }
+       System.out.println(sum);
+
+       double gjennomsnitt1 = sum1/ (double) teller1;
+       System.out.println(gjennomsnitt1);
+
+    }
 }
---
+++ EksamenRepetisjon/src/Oppgave1Igjen.java
@@ -0,0 +1,30 @@
+public class Oppgave1Igjen {
+
+    public static void main(String[] args) {
+
+        int sum = 0;
+        int teller = 0;
+        for (int i = 5; i <= 100; i+=5){
+            sum +=i;
+            teller++;
+
+        }
+        System.out.println(sum);
+        double gjennomsnitt = sum / (double) teller;
+        System.out.println(gjennomsnitt);
+
+
+        int sum1 = 0;
+        int j = 5;
+        int teller1 = 0;
+
+        while (j <= 100){
+            sum1 += j;
+            teller1++;
+            j+=5;
+        }
+        System.out.println(sum1);
+        double gjennomsnitt1 = sum1 / (double) teller1;
+        System.out.println(gjennomsnitt1);
+    }
+}
