ade07e2869fcf78a210f5259d3419647ac8dfbd0
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-12-09 13:44:57 +0100

    oppgaver

--- EksRep/src/Oppgave1.java
+++ EksRep/src/Oppgave1.java
@@ -1,2 +1,13 @@
 public class Oppgave1 {
+
+    public static void main(String[] args) {
+
+        int [] liste = {20, 34, -4, 4, 5, 11, -23};
+
+        for (int i = 0; i < liste.length; i++){
+            System.out.print(liste[i] + " ");
+        }
+
+
+    }
 }
