7e1f7e9eeebf8cb3f1db8dc7895795ff78e7b052
duaaadalh <142824887+duaaadalh@users.noreply.github.com>
2023-12-11 20:44:58 +0100

    oppgave2

--- Eksamen2018/src/Oppgave1.java
+++ Eksamen2018/src/Oppgave1.java
@@ -5,5 +5,11 @@ public class Oppgave1 {
         for (int i = 1; i <=25; i++){
             System.out.println(i);
         }
+
+        int i = 1;
+        while (i <=25){
+            System.out.println(i);
+            i++;
+        }
     }
 }
